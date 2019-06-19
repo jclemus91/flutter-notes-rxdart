da1ab19952afea4d75abcdaccda759c3de2c3094
Julio Lemus <julio.lemus@accenture.com>
2019-06-19 10:38:50 -0500

    adding simple iOS block

--- notes_rx/notes/lib/main.dart
+++ notes_rx/notes/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:notes/ui/list/notes.dart';
 
@@ -21,38 +22,28 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.green,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: NotesScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+// iOS Block
+/* class _MyiOSPage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<_MyiOSPage> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData.light(),
-      home: Scaffold(
-        body: NotesScreen(),
-      ),
+    return CupertinoApp(
+      home: CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(middle: Text("Simple title"),),
+          child: Center(
+        child: CupertinoButton(child: Text("button"), onPressed: null),
+      )),
     );
   }
 }
+*/
